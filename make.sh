ab3a4ffcc85061a8bff96600c742cd9eaa6d24e0
KolenCheung <christian.kolen@gmail.com>
2016-04-11 05:19:45 -0700

    Improve the use of hyperref to mimics the one published in arxivs; creates a lot of variants of the same thing
    11pt vs 12pt; bluelink vs not; alternative metric signature (+,-,-,-)
    then the one chosen in the paper

--- make.sh
+++ make.sh
@@ -1,6 +1,71 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
-latexmk primerv7.tex
-dvips primerv7.dvi
-ps2pdf primerv7.ps
+
+cp primerv7.tex temp.tex
+latexmk temp.tex
+dvips temp.dvi
+ps2pdf temp.ps
+mv temp.pdf primerv7.pdf
+find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
+
+cp primerv7.tex temp.tex
+sed -i '' s/11pt/12pt/ temp.tex
+latexmk temp.tex
+dvips temp.dvi
+ps2pdf temp.ps
+mv temp.pdf primerv7_12pt.pdf
+find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
+
+cp primerv7.tex temp.tex
+sed -i '' s/'%color '// temp.tex
+latexmk temp.tex
+dvips temp.dvi
+ps2pdf temp.ps
+mv temp.pdf primerv7_bluelink.pdf
+find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
+
+cp primerv7.tex temp.tex
+sed -i '' s/11pt/12pt/ temp.tex
+sed -i '' s/'%color '// temp.tex
+latexmk temp.tex
+dvips temp.dvi
+ps2pdf temp.ps
+mv temp.pdf primerv7_12pt_bluelink.pdf
+find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
+
+cp primerv7.tex temp.tex
+sed -i '' s/'\\def\\signofmetric{1}'/'\\def\\signofmetric{0}'/ temp.tex
+latexmk temp.tex
+dvips temp.dvi
+ps2pdf temp.ps
+mv temp.pdf primerv7_altmetric.pdf
+find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
+
+cp primerv7.tex temp.tex
+sed -i '' s/11pt/12pt/ temp.tex
+sed -i '' s/'\\def\\signofmetric{1}'/'\\def\\signofmetric{0}'/ temp.tex
+latexmk temp.tex
+dvips temp.dvi
+ps2pdf temp.ps
+mv temp.pdf primerv7_12pt_altmetric.pdf
+find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
+
+cp primerv7.tex temp.tex
+sed -i '' s/'%color '// temp.tex
+sed -i '' s/'\\def\\signofmetric{1}'/'\\def\\signofmetric{0}'/ temp.tex
+latexmk temp.tex
+dvips temp.dvi
+ps2pdf temp.ps
+mv temp.pdf primerv7_bluelink_altmetric.pdf
+find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
+
+cp primerv7.tex temp.tex
+sed -i '' s/11pt/12pt/ temp.tex
+sed -i '' s/'%color '// temp.tex
+sed -i '' s/'\\def\\signofmetric{1}'/'\\def\\signofmetric{0}'/ temp.tex
+latexmk temp.tex
+dvips temp.dvi
+ps2pdf temp.ps
+mv temp.pdf primerv7_12pt_bluelink_altmetric.pdf
+find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
