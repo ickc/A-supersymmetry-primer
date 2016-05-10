7ea572e21fb49830d4abe2b4697d60a5c5fc0111
KolenCheung <christian.kolen@gmail.com>
2016-05-09 19:33:42 -0700

    Improve the HTML output script

--- make-html.sh
+++ make-html.sh
@@ -1,24 +1,47 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
+cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp primerv7.tex temp.tex
+CONTENT=$(<primerv7.tex)
 
-# String substitution to replace beq and eeq
-sed -i '' s/'\\def\\beq{\\begin{align}}'// temp.tex
-sed -i '' s/'\\def\\eeq{\\end{align}}'// temp.tex
+# Turning non-standard LaTeX syntax into standard ones (e.g. def to newcommand, beq to begin eqnarray)
+CONTENT=$(echo "$CONTENT" | sed \
+	-e s/'\\def\\beq{\\begin{eqnarray}}'// \
+	-e s/'\\def\\eeq{\\end{eqnarray}}'// \
+	-e s/'\\def\\beq{\\begin{eqnarray*}}'// \
+	-e s/'\\def\\eeq{\\end{eqnarray*}}'// \
+	-e s/'\\beq'/'\\begin{eqnarray}'/g \
+	-e s/'\\eeq'/'\\end{eqnarray}'/g \
+	-e 's/\\def\([^{]*\)/\\newcommand{\1}/g' \
+	-e s/'\\newcommand{\\thefootnote}'/'\\renewcommand{\\thefootnote}'/g \
+	-e s/'\\newcommand{\\G}'/'\\renewcommand{\\G}'/g \
+	-e s/'{\\centeron#1#2}'/'{\\centeron}[2]'/g \
+	-e s/'{\\slashchar#1}'/'{\\slashchar}[1]'/g)
 
-sed -i '' s/'\\beq'/'\\begin{eqnarray}'/g temp.tex
-sed -i '' s/'\\eeq'/'\\end{eqnarray}'/g temp.tex
+# ##############################################################################################################
+# ## arXiv's hyperref setting
+# sed -i '' '3 i\
+# \\usepackage[dvips,hypertexnames=false,pdfpagelabels,bookmarksnumbered,linktocpage=true]{hyperref}
+# ' temp.tex
+# ## arXiv's version is 11pt
+# sed -i '' s/12pt/11pt/ temp.tex
+# ## LaTeX to PDF
+# latexmk temp.tex
+# dvips temp.dvi
+# ps2pdf temp.ps
+# mv temp.pdf primerv7-test.pdf
+# mv temp.tex primerv7-test.tex
+# ## Cleanup
+# find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
+# ##############################################################################################################
+
+# debug
+# echo "$CONTENT" > primerv7-normalized.tex
 
 # TeX to pandoc md
-pandoc -t markdown --normalize -s --wrap=none --atx-headers -o primerv7.md temp.tex
+echo "$CONTENT" | pandoc -f latex -t markdown --normalize -s --wrap=none --atx-headers -o primerv7.md
 
-# String substitution: undo pandoc's replacement of eqnarray by aligned
+# undo pandoc's replacement of eqnarray by aligned
 sed -i '' s/'aligned'/'eqnarray'/g primerv7.md
 
 # md to html
-pandoc -f markdown+mmd_header_identifiers+abbreviations+autolink_bare_uris+mmd_link_attributes+markdown_attribute+mmd_title_block+tex_math_double_backslash -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_CHTML-full -t html5 -o primerv7.html primerv7.md
-
-# Cleanup
-find . -iname 'temp.*' -exec mv '{}' ~/.Trash/ \;
+pandoc -S --base-header-level=1 --toc --toc-depth=6 -N --normalize -s --mathjax=https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS_CHTML-full -t html5 -o primerv7.html primerv7.md
